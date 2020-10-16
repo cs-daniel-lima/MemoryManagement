fda238f322c4691abf77dc96f093654d68e8d148
Daniel Lima <daniel.da.cunha.lima@accenture.com>
2020-10-16 14:02:52 -0300

    initial setup

---
+++ projects/Instruments/Instruments/YellowViewController.swift
@@ -0,0 +1,21 @@
+//
+//  YellowViewController.swift
+//  Instruments
+//
+//  Created by daniel.da.cunha.lima on 16/10/20.
+//  Copyright © 2020 concrete. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    deinit {
+        print("YellowViewController is being dealocated")
+    }
+
+}
